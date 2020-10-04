080ea292ad093ae1b93832b54acb800eda5f254c
Madhubabu <madhubabu232343@gmail.com>
2020-10-04 10:12:12 +0530

    Commiting Use Case 5 Files

--- src/main/java/MaximumProblem/MaxMain.java
+++ src/main/java/MaximumProblem/MaxMain.java
@@ -23,16 +23,16 @@ public class MaxMain <A,B,C extends Comparable>{
 	}
 	
 	public  <E extends Comparable<E>> E FindMaximum(A a2,B b2,C c2) {
-     E max = (E) a2;
-     if(((E)b2).compareTo(max)>0)
-    	 max=(E) b2;
-     if(((E)c2).compareTo(max)>0)
-    	 max=(E) c2;
-     return(max);
+	     E max = (E) a2;
+	     if(((E)b2).compareTo(max)>0)
+		 max=(E) b2;
+	     if(((E)c2).compareTo(max)>0)
+		 max=(E) c2;
+	     return(max);
     }
 
 	public <E extends Comparable<E>> E FindMaximum(E[] e) {	
-		int n = e.length;
+	    int n = e.length;
 	    for (int i = 0; i < n-1; i++){
 	        for (int j = 0; j < n-i-1; j++){
 	             if (e[j].compareTo(e[j+1]) > 0) {
@@ -42,14 +42,14 @@ public class MaxMain <A,B,C extends Comparable>{
 	             }
 	         }
 	    }
-	 E max = e[n-1];
-     printMax(max);
-     return(max);
+	    E max = e[n-1];
+	    printMax(max);
+	    return(max);
     }
 	
 	public <E> void printMax(E e)
 	{
-		System.out.println("The Max is "+e);
+	    System.out.println("The Max is "+e);
 	}
 
 }
