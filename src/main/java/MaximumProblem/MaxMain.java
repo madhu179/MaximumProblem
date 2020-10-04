04ead2e6dccf37862ad4732305a42fc825095368
madhu179 <madhubabu232343@gmail.com>
2020-10-04 10:02:37 +0530

    Commiting Use Case 2 Files

--- src/main/java/MaximumProblem/MaxMain.java
+++ src/main/java/MaximumProblem/MaxMain.java
@@ -10,4 +10,13 @@ public class MaxMain {
      return(max);
     }
 
+	
+	public Float FindMaximum(Float a,Float b,Float c) {
+	     Float max = a;
+	     if(b.compareTo(max)>0)
+	    	 max=b;
+	     if(c.compareTo(max)>0)
+	    	 max=c;
+	     return(max);
+	    }
 }
--- src/test/java/MaximumProblem/MaxTest.java
+++ src/test/java/MaximumProblem/MaxTest.java
@@ -14,16 +14,28 @@ public class MaxTest {
 		maxMainObj = new MaxMain();
 	}
 	
-	@Test public void Test_Max_at_Position1() {       
+	@Test public void Integer_Test_Max_at_Position1() {       
         assertEquals(new Integer(3), maxMainObj.FindMaximum(3,2,1));
     }
 	
-	@Test public void Test_Max_at_Position2() {       
+	@Test public void Integer_Test_Max_at_Position2() {       
         assertEquals(new Integer(3), maxMainObj.FindMaximum(2,3,1));
     }
 	
-	@Test public void Test_Max_at_Position3() {       
+	@Test public void Integer_Test_Max_at_Position3() {       
         assertEquals(new Integer(3), maxMainObj.FindMaximum(1,2,3));
+    }
+	
+	@Test public void Float_Test_Max_at_Position1() {       
+        assertEquals(new Float(3), maxMainObj.FindMaximum(3f,2f,1f));
+    }
+	
+	@Test public void Float_Test_Max_at_Position2() {       
+        assertEquals(new Float(3), maxMainObj.FindMaximum(2f,3f,1f));
+    }
+	
+	@Test public void Float_Test_Max_at_Position3() {       
+        assertEquals(new Float(3), maxMainObj.FindMaximum(1f,2f,3f));
     }
 
 }
